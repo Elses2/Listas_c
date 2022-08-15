4dcb6f0b2b9a8a277d7c43deb780f1f832ade03c
eber <eberchiecher2@gmail.com>
2022-08-15 19:34:11 -0300

    creo funciones de creacion de estructuras- tarea-trabajador

--- main.c
+++ main.c
@@ -97,27 +97,108 @@ struct Tarea{
 };
 struct Trabajador{
     int dni;
-    struct Tarea tareas[5];//maximo 5 tareas por persona
+    struct Tarea *tareas[5];//maximo 5 tareas por persona
     bool estres;// si esta estresado o no
     int lugar;//poder ejecutivo judicial, ejecutivo o prensa
     int partido ;// partidos politicos
     int comp_max;//la complejidad masa alta de todas las tareas que tiene
     int cant_t;//cantidad de tareas
-    
+
 
 };
 struct Nodo{
-    struct Trabajador trabajador;
+    struct Trabajador dato;
     struct Nodo *ptrsig;
 
 };
-typedef struct Nodo nodo; 
+typedef struct Nodo nodo;
 typedef nodo *newptr;//puntero nodo
 typedef struct Tarea tarea;
 typedef struct Trabajador trabajador;
+
+
+
+////funciones de estructuras:
+
+///esta funcio crea un nodo que contiene un trabajador
+nodo* crear_nodo(trabajador dato){
+    newptr nuevo;
+    nuevo= malloc(sizeof(nodo));
+    if(nuevo!=NULL){
+    nuevo->dato=dato;
+    }
+
+}
+
+///Esta funcion devuelve una funcion tipo tarea--faltan aplicar controladores
+tarea* crear_tarea (){///n es el numero de tareas
+
+    tarea* nuevo=NULL;
+    int complejidad=0;
+    char descripcion[100];
+
+
+        printf("Dime la descripcion de la tara: \n");
+        gets(descripcion);
+        printf("Dime la complejidad de esta tara: \n");
+        scanf("%d",&complejidad);
+        fflush(stdin);
+
+        nuevo= malloc(sizeof(tarea));
+        nuevo->comp=complejidad;
+        strcpy(nuevo->des, descripcion);
+
+        return nuevo;
+}
+///esta funcion crea un trabajador-- faltan controladores
+
+trabajador * crear_trabajador(){
+    trabajador * nuevo=NULL;
+    nuevo= malloc(sizeof(trabajador));
+
+    int dni;
+    struct Tarea *tareas[5];
+    bool estres=false;
+    int lugar;
+    int partido ;
+    int comp_max;
+    int cant_t;
+
+    printf("Dame el dni\n");
+    scanf("%d",&dni);
+    printf("Dame el lugar 1:poder tal, 2:tal,3:tal,4:tal\n");
+    scanf("%d",&lugar);
+    printf("Dame el dni\n");
+    scanf("%d",&dni);
+    printf("Dame el partido 1: 2: 3: 4:\n");
+    scanf("%d",&partido);
+    printf("Dime la cantidad de tareas que tiene este trabajador:\n");
+    scanf("%d",&cant_t);
+    for(int i=0; i<cant_t; i++){
+
+        tareas[i]=malloc(sizeof(tarea));
+        tareas[i]=crear_tarea();
+        nuevo->tareas[i]=tareas[i];
+        ///si la complejidad de una tarea s mayor de 5 esta estresado osea estres es true
+        if(tareas[i]->comp>5){
+
+            estres=true;
+        }
+
+    }
+    nuevo->cant_t=cant_t;
+    nuevo->comp_max=comp_max;
+    nuevo->dni=dni;
+    nuevo->estres=estres;
+    nuevo->lugar=lugar;
+    nuevo->partido=partido;
+    return nuevo;
+}
+
+
 int main()
 {
-
+    tarea *prueba=NULL;
     int n=2;
     char boton_c [20];
     int boton=0;
@@ -133,7 +214,7 @@ int main()
 
 
         switch(boton){
-        case 1: 
+        case 1:
             break;
         case 2:
             break;
